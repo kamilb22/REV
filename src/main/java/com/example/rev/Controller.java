1bceefe5bb293294a5d9dbc01cb846afe6cb3cfc
kamilb22 <222574@edu.p.lodz.pl>
2021-10-07 13:59:56 +0200

    Added Controller with reverse method

---
+++ src/main/java/com/example/rev/Controller.java
@@ -0,0 +1,14 @@
+package com.example.rev;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Controller {
+
+    @RequestMapping("/{value}")
+    public String reverse(@PathVariable String value) {
+        return "";
+    }
+}
